4e4b0c185cae5b5cd3606040cea4c350299f9a04
Łukasz Przybylski <lukaszprzybylskidev@gmail.com>
2025-12-20 22:29:51 +0000

    chore: set sqlite as default database config

--- config/configs/database.php
+++ config/configs/database.php
@@ -9,15 +9,12 @@ return [
     "default_connection" => "default",
     "connections" => [
         "default" => [
-            "driver" => $env->getString("DB_DRIVER", "pdo_mysql"),
-            "host" => $env->getString("DB_HOST", "127.0.0.1"),
-            "port" => $env->getInt("DB_PORT", 3306),
-            "database" => $env->getString("DB_NAME", "lpwork"),
-            "username" => $env->getString("DB_USER", "root"),
-            "password" => $env->getString("DB_PASSWORD", ""),
+            "driver" => $env->getString("DB_DRIVER", "pdo_sqlite"),
+            "path" => $env->getString(
+                "DB_PATH",
+                \dirname(__DIR__, 2) . "/storage/db.sqlite3",
+            ),
             "charset" => $env->getString("DB_CHARSET", "utf8mb4"),
-            "url" => $env->getString("DB_URL", ""),
-            "path" => $env->getString("DB_PATH", ""),
         ],
         // Additional named connections can be added here.
     ],
