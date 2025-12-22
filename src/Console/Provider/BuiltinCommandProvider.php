9a8a9b7f23eee85f7af48b564e8c3a343cef4250
Łukasz Przybylski <lukaszprzybylskidev@gmail.com>
2025-12-22 19:52:35 +0000

    chore: remove informational route and database commands

--- src/Console/Provider/BuiltinCommandProvider.php
+++ src/Console/Provider/BuiltinCommandProvider.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 namespace LPwork\Console\Provider;
 
 use LPwork\Console\Command\MigrateCommand;
-use LPwork\Console\Command\DatabaseConnectionsCommand;
 use LPwork\Console\Command\MigrateFreshCommand;
-use LPwork\Console\Command\RoutesListCommand;
 use LPwork\Console\Command\VersionCommand;
 use LPwork\Console\Command\CacheWarmCommand;
 use LPwork\Console\Command\CacheClearCommand;
@@ -40,9 +38,7 @@ class BuiltinCommandProvider implements CommandProviderInterface
     {
         return [
             $this->container->get(MigrateCommand::class),
-            $this->container->get(DatabaseConnectionsCommand::class),
             $this->container->get(MigrateFreshCommand::class),
-            $this->container->get(RoutesListCommand::class),
             $this->container->get(VersionCommand::class),
             $this->container->get(CacheWarmCommand::class),
             $this->container->get(CacheClearCommand::class),
